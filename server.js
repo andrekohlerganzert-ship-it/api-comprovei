08a59dc947ce61234d32477386844f8be6cb06c3
andrekohlerganzert-ship-it <andre.kohler.ganzert@gmail.com>
2026-06-03 09:02:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-pp.get('/teste-env', (req, res) => {
+app.get('/teste-env', (req, res) => {
     res.json({
         usuario: process.env.COMPROVEI_USER ? 'OK' : 'VAZIO',
         senha: process.env.COMPROVEI_PASS ? 'OK' : 'VAZIO'
